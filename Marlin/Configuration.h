43e16c86e22af04230e5021d2bd232749cac1264
Emmanuel Brendel <emmanuel@accelys.net>
2018-12-17 10:54:33 +0100

    Sets PLA PREHEAT_1_TEMP_BED to 60

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1263,7 +1263,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
