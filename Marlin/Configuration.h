cb1edbead6c404a92cd229ab1a6d25ec7710a3ff
Emmanuel Brendel <emmanuel@accelys.net>
2018-12-17 09:18:01 +0100

    Uses PID found somewhere, to be improved.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -379,10 +379,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
+  // Tronxy
+  #define  DEFAULT_Kp 24.06
+  #define  DEFAULT_Ki 2.10
+  #define  DEFAULT_Kd 68.98
+
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
